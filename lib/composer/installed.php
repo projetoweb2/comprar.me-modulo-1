19d67d3237b787205ea0f15837936888baf05d4b
rafael <arquivoweyne@gmail.com>
2020-09-03 15:21:20 -0300

    instalando o composer no projeto

---
+++ lib/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cdbe2e7eac2f24422dfc15c80d3fee58cb05cf9d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cdbe2e7eac2f24422dfc15c80d3fee58cb05cf9d',
+    ),
+    'smarty/smarty' => 
+    array (
+      'pretty_version' => 'v3.1.36',
+      'version' => '3.1.36.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd148f7ade295014fff77f89ee3d5b20d9d55451',
+    ),
+  ),
+);
