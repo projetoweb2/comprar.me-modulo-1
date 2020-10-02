b9540c43aff095384d21e108c6fd8ecb03c9e153
rafael <arquivoweyne@gmail.com>
2020-10-02 09:45:39 -0300

    Explode para separar as URL's

--- model/Rotas.class.php
+++ model/Rotas.class.php
@@ -1,11 +1,18 @@
 <?php 
 
 Class Rotas{
+
+	public static $pag;
     
     //Método estático pode ser acessado sem chamar a classe
 	static function get_Pagina(){
 		//Verifica se a URL chamou o arquivo.
 		if(isset($_GET['pag'])){
+
+			$pagina = $_GET['pag'];
+
+			self::$pag = explode('/', $pagina);
+
 			//Concatena ele com .php
 			$pagina = 'controller/' .$_GET['pag'] . '.php';
 			echo $pagina;
