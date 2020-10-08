41823c990fbff82683ca1ef515dc5f1bb19b40e7
rafael <arquivoweyne@gmail.com>
2020-10-08 20:38:01 -0300

    Constantes para o banco de dados

--- model/Config.class.php
+++ model/Config.class.php
@@ -1,8 +1,22 @@
 <?php 
 
-const SITE_URL = "http://localhost";
-const SITE_PASTA = "loja";
-const SITE_NOME = "comprar.me";
-const SITE_EMAIL_ADM = "rafaellucasdesenvolvedorweb@gmail.com"
+Class Config(){
+
+	//INFORMAÇÕES BÁSICAS DO SITE.
+	const SITE_URL = "http://localhost";
+	const SITE_PASTA = "loja";
+	const SITE_NOME = "comprar.me";
+	const SITE_EMAIL_ADM = "rafaellucasdesenvolvedorweb@gmail.com";
+
+	//INFORMAÇÕES DO BANCO DE DADOS.
+	const BD_HOST = "localhost",
+	      BD_USER = "root",
+	      BD_SENHA = "",
+	      BD_BANCO = "comprar.me",
+
+	//INFORMAÇÕES PARA O PHP MAILER.
+	const EMAIL_HOST = "smtp.gmail.com";
+
+}
 
  ?>
